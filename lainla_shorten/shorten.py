76be76e9581f6958e63a1de2d74b23044a11163e
NecRaul <necraul2001@gmail.com>
2023-08-29 03:24:56 +0400

    Shorten request

---
+++ lainla_shorten/shorten.py
@@ -0,0 +1,15 @@
+import requests
+
+def shorten_url(url):
+    
+    data = f'url={url}'
+    
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+    
+    response = requests.post('https://s.lain.la', headers=headers, data=data)
+    
+    return response.text
+
+print(shorten_url(input("URL: ")))
