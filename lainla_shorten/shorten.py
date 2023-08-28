5ef3c6d40e782b16e1ad0d99b80e594274763188
NecRaul <necraul2001@gmail.com>
2023-08-29 03:31:50 +0400

    Checking url validity with regex

--- lainla_shorten/shorten.py
+++ lainla_shorten/shorten.py
@@ -1,6 +1,23 @@
 import requests
+import re
+
+def url_check(url):
+    pattern = r"^https?://(?:www\.)?[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+    return re.match(pattern, url) is not None
 
 def shorten_url(url):
+    
+    # checking if the url is valid
+    is_valid_url = url_check(url)
+    
+    url = f"https://{url}" if not is_valid_url else url
+    
+    # checking if the url is valid after
+    # adding https:// to the start of the string
+    is_valid_url = url_check(url)
+    
+    if (not is_valid_url):
+        return "Not a valid url."
     
     data = f'url={url}'
     
