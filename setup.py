0242346e9de9bbf14259dfda7c1318bf69fd03ee
NecRaul <necraul@kuroneko.dev>
2023-08-29 09:37:10 +0400

    Production/Stable

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.0.2"
+VERSION = "1.0.0"
 DESCRIPTION = "URL Shortener using s.lain.la API."
 LONG_DESCRIPTION = "s.lain.la can shorten URLs using CUrl. I tend to forget the CUrl syntax and arguments quite easily so I made a Python package to streamline things."
 AUTHOR = "NecRaul"
@@ -15,7 +15,7 @@ setup(
     install_requires=["requests", "pyperclip", "setuptools"],
     keywords=["python", "link shortener", "shortener", "lain", "lain.la", "s.lain.la"],
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
@@ -23,7 +23,6 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
         "Topic :: Internet :: WWW/HTTP"
-        
     ],
     py_modules=["shorten"],
     entry_points={
