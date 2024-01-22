cb83053986d641b12ddf97d36fbaa7b9205c5145
NecRaul <necraul@kuroneko.dev>
2024-01-22 10:57:12 +0400

    Changed version to 1.7

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "1.6"
+VERSION = "1.7"
 DESCRIPTION = "URL Shortener using s.lain.la API."
 with open("README.md", "r") as file:
     LONG_DESCRIPTION = file.read()
